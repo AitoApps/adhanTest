3e944efb89b227fe1bdf1768336e7a14aa09a9e1
redoine <androidoine@gmail.com>
2016-08-04 21:52:25 +0100

    test adan

--- AdhanApp/ViewController.swift
+++ AdhanApp/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import Adhan
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var fajerTime: UILabel!
+    @IBOutlet weak var doherTime: UILabel!
+    @IBOutlet weak var asserTime: UILabel!
+    @IBOutlet weak var maghribTime: UILabel!
+    @IBOutlet weak var aichaaTime: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let date = cal.components([.Year, .Month, .Day], fromDate:NSDate())
+        let coordinates = Coordinates(latitude: 35.78056, longitude: -78.6389)
+        var params = CalculationMethod.MoonsightingCommittee.params
+        params.madhab = .Hanafi
+        if let prayers = PrayerTimes(coordinates: coordinates, date: date, calculationParameters: params) {
+            let formatter = NSDateFormatter()
+            formatter.timeStyle = .MediumStyle
+            formatter.timeZone = NSTimeZone(name: "America/New_York")!
+            
+            fajerTime.text = formatter.stringFromDate(prayers.fajr)
+            doherTime.text = formatter.stringFromDate(prayers.dhuhr)
+            asserTime.text = formatter.stringFromDate(prayers.asr)
+            maghribTime.text = formatter.stringFromDate(prayers.maghrib)
+            aichaaTime.text = formatter.stringFromDate(prayers.isha)
+            
+            NSLog("fajr %@", formatter.stringFromDate(prayers.fajr))
+            NSLog("sunrise %@", formatter.stringFromDate(prayers.sunrise))
+            NSLog("dhuhr %@", formatter.stringFromDate(prayers.dhuhr))
+            NSLog("asr %@", formatter.stringFromDate(prayers.asr))
+            NSLog("maghrib %@", formatter.stringFromDate(prayers.maghrib))
+            NSLog("isha %@", formatter.stringFromDate(prayers.isha))
+        }
+        
+        
+   
     }
 
     override func didReceiveMemoryWarning() {
